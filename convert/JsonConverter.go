26ddc602f3cf5534b998eca48769ea80fd56965b
Dmitrii Uzdemir <uzdemir.dmt@gmail.com>
2022-04-30 14:51:38 +0400

    fix JsonConvertor

--- convert/JsonConverter.go
+++ convert/JsonConverter.go
@@ -14,22 +14,28 @@ import (
 //  fmt.Println(value1) // map[key:123]
 //  fmt.Println(value2) // map[key:123]
 //  fmt.Println(value3) // {"key":123}
-var JsonConverter = &_TJsonConverter{
-	_jsonEngine: &defaultJsonEngine{},
+var JsonConverter = &_TJsonConverter[any]{
+	_jsonEngine: &defaultJsonEngine[any]{},
 }
 
-type _TJsonConverter struct {
-	_jsonEngine IJSONEngine
+type _TJsonConverter[T any] struct {
+	_jsonEngine IJSONEngine[T]
 }
 
 // IJSONEngine interface which helps to marshal and unmarshal json
-type IJSONEngine interface {
-	FromJson(value string) (any, error)
-	ToJson(value any) (string, error)
+type IJSONEngine[T any] interface {
+	FromJson(value string) (T, error)
+	ToJson(value T) (string, error)
+}
+
+func NewDefaultCustomTypeJsonConvertor[T any]() IJSONEngine[T] {
+	return &_TJsonConverter[T]{
+		_jsonEngine: &defaultJsonEngine[T]{},
+	}
 }
 
 // SetUpJSONEngine allows overriding JSON engine
-func (j *_TJsonConverter) SetUpJSONEngine(jsonEngine IJSONEngine) bool {
+func (j *_TJsonConverter[T]) SetUpJSONEngine(jsonEngine IJSONEngine[T]) bool {
 	if jsonEngine == nil {
 		return false
 	}
@@ -40,32 +46,32 @@ func (j *_TJsonConverter) SetUpJSONEngine(jsonEngine IJSONEngine) bool {
 // FromJson converts value from JSON string
 // Parameters: "value" - the JSON string to convert.
 // Returns: converted object value or null when value is null.
-func (j *_TJsonConverter) FromJson(value string) (any, error) {
+func (j *_TJsonConverter[T]) FromJson(value string) (T, error) {
 	return j._jsonEngine.FromJson(value)
 }
 
 // ToJson converts value into JSON string.
 // Parameters: "value" - the value to convert.
 // Returns: JSON string or null when value is null.
-func (j *_TJsonConverter) ToJson(value any) (string, error) {
+func (j *_TJsonConverter[T]) ToJson(value T) (string, error) {
 	return j._jsonEngine.ToJson(value)
 }
 
 // ToNullableMap converts JSON string into map object or returns null when conversion is not possible.
 // Parameters: "value" - the JSON string to convert.
 // Returns: Map object value and true or null and false when conversion is not supported.
-func (j *_TJsonConverter) ToNullableMap(value string) (map[string]any, bool) {
-	v, _ := j.FromJson(value)
-	if v == nil {
+func (j *_TJsonConverter[T]) ToNullableMap(value string) (map[string]any, bool) {
+	if v, err := j.FromJson(value); err != nil {
 		return nil, false
+	} else {
+		return toNullableMap(v)
 	}
-	return toNullableMap(v)
 }
 
 // ToMap converts JSON string into map object or returns empty map when conversion is not possible.
 // Parameters: "value" - the JSON string to convert.
 // Returns: Map object value or empty map when conversion is not supported.
-func (j *_TJsonConverter) ToMap(value string) map[string]any {
+func (j *_TJsonConverter[T]) ToMap(value string) map[string]any {
 	return j.ToMapWithDefault(value, map[string]any{})
 }
 
@@ -74,7 +80,7 @@ func (j *_TJsonConverter) ToMap(value string) map[string]any {
 //  "value" - the JSON string to convert.
 //  "defaultValue" - the default value.
 // Returns: Map object value or default map when conversion is not supported.
-func (j *_TJsonConverter) ToMapWithDefault(value string, defaultValue map[string]any) map[string]any {
+func (j *_TJsonConverter[T]) ToMapWithDefault(value string, defaultValue map[string]any) map[string]any {
 	if m, ok := j.ToNullableMap(value); ok {
 		return m
 	}
@@ -82,34 +88,31 @@ func (j *_TJsonConverter) ToMapWithDefault(value string, defaultValue map[string
 }
 
 // jsonEngine is a default json converter engine
-type defaultJsonEngine struct{}
+type defaultJsonEngine[T any] struct{}
 
 // FromJson converts value from JSON string
-// Parameters: "value" - the JSON string to convert.
-// Returns: converted object value or null when value is null.
-func (je defaultJsonEngine) FromJson(value string) (any, error) {
+//	Parameters: "value" - the JSON string to convert.
+//	Returns: converted object value or null when value is null.
+func (je defaultJsonEngine[T]) FromJson(value string) (T, error) {
+	var defaultValue T
 	if value == "" {
-		return nil, nil
+		return defaultValue, nil
 	}
 
-	var m any
+	var m T
 	if err := json.Unmarshal([]byte(value), &m); err != nil {
-		return nil, err
+		return defaultValue, err
 	}
 	return m, nil
 }
 
 // ToJson converts value into JSON string.
-// Parameters: "value" - the value to convert.
-// Returns: JSON string or null when value is null.
-func (je defaultJsonEngine) ToJson(value any) (string, error) {
-	if value == nil {
-		return "", nil
-	}
-
+//	Parameters: "value" - the value to convert.
+//	Returns: JSON string or null when value is null.
+func (je defaultJsonEngine[T]) ToJson(value T) (string, error) {
 	b, err := json.Marshal(value)
 	if err != nil {
 		return "", err
 	}
-	return string(b[:]), nil
+	return string(b), nil
 }
